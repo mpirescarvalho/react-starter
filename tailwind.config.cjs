f200f340818c7bbb0696d51609ec13e726741be2
Marcelo Carvalho <mpirescarvalho17@gmail.com>
2022-12-07 07:44:02 -0300

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,40 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      '2xl': 32,
+    },
+
+    colors: {
+      transparent: 'transparent',
+
+      black: '#000',
+      white: '#fff',
+
+      gray: {
+        100: '#e1e1e6',
+        200: '#c4c4cc',
+        400: '#7c7c8a',
+        800: '#202024',
+        900: '#121214',
+      },
+
+      cyan: {
+        300: '#9be1fb',
+        500: '#81d8f7',
+      },
+    },
+    extend: {
+      fontFamily: {
+        sans: 'Inter, sans-serif',
+      },
+    },
+  },
+  plugins: [],
+};
